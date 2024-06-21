b726d7e8155dfb64137ebdc324d4c9b7f5f9a146
Diivvuu <divyanshubawa9@gmail.com>
2024-06-21 12:38:10 +0530

    errors done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,31 @@
 import React from "react";
+import useFormValidation from "../hooks/useFormValidation";
 //fields - name, email, age, are you ateend with a guest?, guest name
+const validateForm = (values) => {
+  const errors = {};
+  if (!values.name)
+    errors.name =
+      "Oops! Looks like you forgot to tell us your name. Can't we at least know who you are?";
+  if (!values.email)
+    errors.email =
+      "Don't leave us hanging! We need your email to keep in touch.";
+  else if (!/\S+@\S+\.\S+/.test(values.email))
+    errors.email =
+      "That doesn't look like a valid email address. Did you mean to type example@example.com?";
+  if (!values.age)
+    errors.age =
+      "We'd love to know your age, Don't worry we won't tell anyone!";
+  else if (isNaN(values.age)) errors.age = "Age must be a number!";
+  else if (values.age <= 0 || values.age >= 150)
+    errors.age = "Are you a time traveller or something?";
+  if (values.attendingWithGuest === "Yes" && !values.guestName)
+    errors.guestName =
+      "Guest name is required, We want to know who you are bringing!";
+
+  return errors;
+};
 const Form = () => {
+  const { formData, errors, handleChange, handleSubmit } = useFormValidation();
   return (
     <div className="h-screen w-full mx-auto bg-zinc-600">
       <form className="flex flex-col justify-center items-center pt-24">
